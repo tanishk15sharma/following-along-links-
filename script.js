1b2717604c6914136c3cce504a1b7552c29cd84a
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-05 00:04:02 +0530

    final script
    I have added the final script which is working good .

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ document.body.append(highlight);
 
 function highlightLink() {
     // console.log(this);
-    const linkCoords = this.getBoundingClientRect();
+    const linkCoords = this.getBoundingClientRect();  //this will give me the height,width,top,bottom for the element selected
     console.log(linkCoords);
-    highlight.style.width = `${linkCoords.width}px `;
-    highlight.style.height = `${linkCoords.height}px `;
+
+    const coordinates = {
+        width : linkCoords.width,
+        height : linkCoords.height,
+        top : linkCoords.top + window.scrollY,   //this will add top/bottom to links when scrolled 
+        left : linkCoords.left + window.scrollX  // this will add right/left if there is a slider 
+    
+    } 
+
+
+
+    highlight.style.width = `${coordinates.width}px `;   //giving reference of height and width 
+    highlight.style.height = `${coordinates.height}px `; //to heighlight to work accordingly 
+
+    highlight.style.transform = `translate(${coordinates.left}px,${coordinates.top}px  `
 }
 
 triggers.forEach(a => a.addEventListener("mouseenter",highlightLink  ) )
